c76ae7040485b38b872f1fae6731f082e0c0824a
taedi90 <taedi90@gmail.com>
2021-12-25 11:05:26 +0900

    프로젝트 초기화

---
+++ src/main/java/net/taedi/restful/RestfulApplication.java
@@ -0,0 +1,13 @@
+package net.taedi.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/taedi/restful/RestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package net.taedi.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
